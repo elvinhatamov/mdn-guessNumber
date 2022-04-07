1a9fcfac8beb37388cbf0c22f696f55918808d33
Elvin Hatamov <58266209+elvinhatamov@users.noreply.github.com>
2022-04-07 01:15:43 -0400

    Initial commit

---
+++ forEach.js
@@ -0,0 +1,6 @@
+const favoriteFlavors =["cookie dough", "salted caramel", "toffee"];
+
+favoriteFlavors.forEach(function(flavour){
+    debugger
+    console.log("I like "+ flavour +"ice cream")
+})
